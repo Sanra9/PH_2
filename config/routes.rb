cbbda98336b877d596d5396fe6d8b12c356c6149
Santi <sanrra999@gmail.com>
2023-10-23 15:08:18 -0500

    Crea la ruta /products/:id/edit

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
   get '/products', to: 'products#index'
   get '/products/new', to: 'products#new', as: 'new_product'
   post '/products', to: 'products#create'
-  get '/products/:id', to: 'products#show'
+  get '/products/:id', to: 'products#show', as: 'product'
+  get '/products/:id/edit', to: 'products#edit'
 
   # Defines the root path route ("/")
   # root "posts#index"
