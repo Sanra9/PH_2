529f11eb2995e70f540c4c57264a69a2474ca3cd
Santi <sanrra999@gmail.com>
2023-11-09 14:31:50 -0500

    Anota los modelos con los campos de la base de datos

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: products
+#
+#  id          :integer          not null, primary key
+#  description :text
+#  name        :string
+#  url         :string
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
 class Product < ActiveRecord::Base 
     validates :name, presence: true
     validates :url, presence: true
