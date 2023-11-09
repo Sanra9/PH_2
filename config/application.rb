556f1f7e954d7789f7094dc0c4fe1c3774d0e5ec
Santi <sanrra999@gmail.com>
2023-11-09 15:55:47 -0500

    Cambia el idioma a español

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Producthunt
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale = :es
+    config.i18n.enforce_available_locales = true
   end
 end
